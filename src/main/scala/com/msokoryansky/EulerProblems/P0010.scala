55f5b6ebd0d703c49c4ff1a8baae8aca6ceb4c23
M S <msokoryansky@yahoo.com>
2017-07-25 09:23:06 -0400

    Problem #10 solved. Tests forthcoming

--- src/main/scala/com/msokoryansky/EulerProblems/P0010.scala
+++ src/main/scala/com/msokoryansky/EulerProblems/P0010.scala
@@ -3,9 +3,55 @@ package com.msokoryansky.EulerProblems
 import scala.annotation.tailrec
 
 class P0010 extends EulerProblem {
+  /**
+    * @param i first integer in the stream
+    * @return Stream of all integers starting with i
+    */
+  def ints(i: Long): Stream[Long] = i #:: ints(i + 1)
 
-  def run: String = "Hi"
+  /**
+    * Test if number is a prime (compares to all 1..SQRT(n))
+    * @param n mumber to check if it's prime
+    * @return if number is prime then true, else false
+    */
+  def isPrime(n: Long): Boolean = {
+    n match {
+      case x if x < 2 => false
+      case _ => !(2 to Math.sqrt(n.toDouble).ceil.toInt).exists{x => x != n && n % x == 0}
+    }
+  }
+
+  /**
+    * Next prime number that's greater than lowerLimit
+    * @param lowerLimit lower cutoff (inclusive) for next prime
+    * @return
+    */
+  def nextPrime(lowerLimit: Long): Long = {
+    if (lowerLimit < 2) 2
+    else {
+      @tailrec def nextPrimeAcc(ints: Stream[Long]): Long = {
+        if (isPrime(ints.head)) ints.head else nextPrimeAcc(ints.tail)
+      }
+      nextPrimeAcc(ints(lowerLimit))
+    }
+  }
+
+  /**
+    * Returns sum of all primes that are less than limit
+    * @param limit cutoff at which we stop summing primes
+    * @return sum of all primes below cutoff
+    */
+  def primeNumberSum(limit: Int): Long = {
+    @tailrec def primeNumberSumAcc(n: Long, acc: Long): Long = {
+      val p = nextPrime(n + 1)
+      if (p >= limit) acc else primeNumberSumAcc(p, acc + p)
+    }
+    primeNumberSumAcc(0, 0)
+  }
+
+  def run: String = primeNumberSum(2000000).toString
 }
+
 
 object P0010 extends App {
   (new P0010).printAnswer()
