0584b6df9d92552be346f26cbdda3cec7bca6c50
M S <msokoryansky@yahoo.com>
2017-07-28 08:36:38 -0400

    #11 done

--- src/main/scala/com/msokoryansky/EulerProblems/P0011.scala
+++ src/main/scala/com/msokoryansky/EulerProblems/P0011.scala
@@ -55,8 +55,8 @@ class P0011 extends EulerProblem {
       |20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
       |01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
     """.stripMargin).
-          bestGridResultInAllDirections(4,
-            SortingHat[Long]((a: Long, b: Long) => a * b, 1, (x: Long, y: Long) => Math.max(x, y), 0))
+    bestGridResultInAllDirections(4,
+      SortingHat[Long]((a: Long, b: Long) => a * b, 1, (x: Long, y: Long) => Math.max(x, y), 0))
     .toString
 }
 
@@ -146,7 +146,7 @@ class NumberGrid private (longGrid: Array[Array[Long]]) {
     */
   def bestCellResultInAllDirections(startX: Int, startY: Int, length: Int, hat: SortingHat[Long]): Long = {
     Direction.values.map{(dir) => nextValuesInDirection(startX, startY, dir, length).foldLeft(hat.calcAcc)(hat.calc)}
-                                                    .foldLeft(hat.selectAcc)(hat.select)
+      .foldLeft(hat.selectAcc)(hat.select)
   }
 
   /**
@@ -158,7 +158,7 @@ class NumberGrid private (longGrid: Array[Array[Long]]) {
   def bestGridResultInAllDirections(length: Int, hat: SortingHat[Long]): Long = {
     def bestGridResultInAllDirectionsAcc(startX: Int, startY: Int, acc: Long): Long = {
       val best = (bestCellResultInAllDirections(startX, startY, length, hat) :: acc :: Nil)
-                    .foldLeft(hat.selectAcc)(hat.select)
+        .foldLeft(hat.selectAcc)(hat.select)
       if (startY >= lengthY - 1 && startX >= lengthX - 1) best
       else {
         val nextX = if (startX < lengthX - 1) startX + 1 else 0
