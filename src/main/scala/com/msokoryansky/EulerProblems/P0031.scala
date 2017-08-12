550bbb449f94a3c72b6068d08399f6c4e1e616a2
M S <msokoryansky@yahoo.com>
2017-08-12 16:16:40 -0400

    putzing around with data structures. No significant improvement on speed. Need to have an algo that more aggressively prunes duplicate trees

--- src/main/scala/com/msokoryansky/EulerProblems/P0031.scala
+++ src/main/scala/com/msokoryansky/EulerProblems/P0031.scala
@@ -17,7 +17,7 @@ How many different ways can £2 be made using any number of coins?
  */
 
 class P0031 extends EulerProblem {
-  def run: String = SumOfParts.waysToAddUpParts(200, Set(1, 2, 5, 10, 20, 50, 100, 200)).size.toString
+  def run: String = SumOfParts.waysToAddUpParts(30, Set(1, 2, 5, 10, 20, 50, 100, 200)).size.toString
 }
 
 object P0031 extends App {
