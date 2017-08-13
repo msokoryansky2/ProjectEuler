52ded2749c25a62c2f6b67f60f68dde986a76ffc
M S <msokoryansky@yahoo.com>
2017-08-13 11:05:19 -0400

    continuing DIY Fraction + tests

--- src/main/scala/com/msokoryansky/MathUtils/Fraction.scala
+++ src/main/scala/com/msokoryansky/MathUtils/Fraction.scala
@@ -5,25 +5,42 @@ import scala.annotation.tailrec
 class Fraction(val num: Long, val denom: Long) extends Ordered[Fraction] {
   require(denom != 0, "Cannot have denominator of 0")
 
+  import scala.math.Ordered.orderingToOrdered
+
   lazy val decimal: Float = num / denom
   lazy val isWhole: Boolean = simplify.denom == 1
 
   override def toString: String = num.toString + "/" + denom.toString
 
-  def compare(other: Fraction): Int = this.num * other.denom - this.denom * other.num match {
-    case x if x >  0 => 1
-    case x if x == 0 => 0
-    case x if x <  0 => -1
+  def compare(that: Fraction): Int = {
+    val this2 = this.simplifyPositiveDenom
+    val that2 = that.simplifyPositiveDenom
+
+    if (this2.num < 0 && that2.num >= 0) -1
+    else if (this2.num > 0 && that2.num <= 0) 1
+    else if (this2.num < 0 && that2.num < 0)
+      -1 * Fraction(Math.abs(this2.num), this2.denom).compare(Fraction(Math.abs(that2.num), that2.denom))
+    else this2.num * that2.denom - this2.denom * that2.num match {
+      case x if x > 0  => 1
+      case x if x == 0 => 0
+      case x if x < 0  => -1
+    }
   }
 
-  def ===(other: Fraction): Boolean = this.num == other.num && this.denom == other.denom
+  def canEqual(a: Any): Boolean = a.isInstanceOf[Fraction]
+  override def equals(that: Any): Boolean =
+    that match {
+      case fraction: Fraction => compare(fraction) == 0
+      case _ => super.equals(that)
+    }
+
+  def simplifyPositiveDenom: Fraction = if (denom < 0) Fraction(0 - num, 0 - denom) else this
 
   def simplify: Fraction = {
     // Get simple cases out of the way quickly
     if (num == 0) Fraction(0, 1)
-    else if (denom < 0) Fraction(0 - num, 0 - denom).simplify
-    else if (num % denom == 0) Fraction(num / denom, 1)
-    else if (denom % num == 0) Fraction(1, denom / num)
+    else if (num % denom == 0) Fraction(num / denom, 1).simplifyPositiveDenom
+    else if (denom % num == 0) Fraction(1, denom / num).simplifyPositiveDenom
     else {
       @tailrec def simplifyAcc(numFactors: List[Long], denomFactors: List[Long], acc: Fraction): Fraction =
         numFactors match {
@@ -34,7 +51,7 @@ class Fraction(val num: Long, val denom: Long) extends Ordered[Fraction] {
         }
       simplifyAcc(Integer.divisors(Math.abs(num)).filter(_ != 1).toList,
                   Integer.divisors(Math.abs(denom)).filter(_ != 1).toList,
-                  this)
+                  this).simplifyPositiveDenom
     }
   }
 }
