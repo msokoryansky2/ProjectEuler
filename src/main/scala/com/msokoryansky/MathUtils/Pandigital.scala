8406ae1ec1b0ff9595aed523ef3c38ee5885d8b3
M S <msokoryansky@yahoo.com>
2017-08-13 19:58:56 -0400

    expanding pandigital logic in prep for #38

--- src/main/scala/com/msokoryansky/MathUtils/Pandigital.scala
+++ src/main/scala/com/msokoryansky/MathUtils/Pandigital.scala
@@ -1,6 +1,9 @@
 package com.msokoryansky.MathUtils
 
+import scala.annotation.tailrec
+
 object Pandigital {
+  def pandigital1To9(i: Long): Boolean = combinedPandigital1To9(List(i))
   def combinedPandigital1To9(l: List[Long]): Boolean = l.mkString.map(_.asDigit).sortWith(_ < _) == (1 to 9).toList
 
   /**
@@ -10,5 +13,19 @@ object Pandigital {
   def multiMultiProductPandigital1To9(i: Long): Boolean = {
     if (i.toString.toList.distinct != i.toString.toList) false
     else Integer.divisors(i).filter(d => d != 1 && d != i).exists(d => combinedPandigital1To9(List(i, d, i / d)))
+  }
+
+  def concatenatedMultiplePandigital1To9(i: Long, r: Range): Boolean = {
+    val string = r.map(re => (re * i).toString).mkString
+    if (string.length != 9) false else pandigital1To9(string.toLong)
+  }
+
+  def concatenatedMultiplePandigital1To9RangeFinder(i: Long): Option[Int] = {
+    @tailrec def concatenatedMultiplePandigital1To9RangeFinderAcc(n: Int): Option[Int] = {
+      if (n > 8) None
+      else if (concatenatedMultiplePandigital1To9(i, 1 to n)) Some(n)
+      else concatenatedMultiplePandigital1To9RangeFinderAcc(n + 1)
+    }
+    concatenatedMultiplePandigital1To9RangeFinderAcc(2)
   }
 }
--- src/test/scala/com/msokoryansky/MathUtils/TestPandigital.scala
+++ src/test/scala/com/msokoryansky/MathUtils/TestPandigital.scala
@@ -10,8 +10,21 @@ class TestPandigital extends FunSuite {
     assert(!Pandigital.combinedPandigital1To9(List(1, 2, 3, 45, 679)))
   }
 
-  test("") {
+  test("multiMultiProductPandigital1To9 tests if a number has two multiples that altogether form a 1-9 pandigital") {
     assert(!Pandigital.multiMultiProductPandigital1To9(12345))
     assert(Pandigital.multiMultiProductPandigital1To9(7254))
+  }
+
+  test("concatenatedMultiplePandigital1To9 tests if a number can be multiplied by a range (1 to n) which forms" +
+    " a 1-9 pandigital when concatenated") {
+    assert(Pandigital.concatenatedMultiplePandigital1To9(192, 1 to 3))
+    assert(!Pandigital.concatenatedMultiplePandigital1To9(191, 1 to 3))
+  }
+
+  test("concatenatedMultiplePandigital1To9RangeFinder finds number n for (1 to n) range, if exists, " +
+    "that makes a number concatenatedMultiplePandigital1To9-ish") {
+    assert(Pandigital.concatenatedMultiplePandigital1To9RangeFinder(192) === Some(3))
+    assert(Pandigital.concatenatedMultiplePandigital1To9RangeFinder(9) === Some(5))
+    assert(Pandigital.concatenatedMultiplePandigital1To9RangeFinder(90) === None)
   }
 }
