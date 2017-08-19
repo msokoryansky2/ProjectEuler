2613a15d5757c32049c8533704656c369eb43788
M S <msokoryansky@yahoo.com>
2017-08-19 14:15:59 -0400

    Break out solution tests into 25 per test file

---
+++ src/test/scala/com/msokoryansky/EulerProblems/TestEulerProblems0051_0075.scala
@@ -0,0 +1,13 @@
+package com.msokoryansky.EulerProblems
+
+import org.scalatest.FunSuite
+
+class TestEulerProblems0051_0075 extends FunSuite {
+  test("#51") {
+    assert((new P0051).run === "121313")
+  }
+
+  test("#52") {
+    assert((new P0052).run === "142857")
+  }
+}
