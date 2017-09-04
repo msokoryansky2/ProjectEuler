0aad5dbda3f64d548fedf3a05c4756e1e4669f05
M S <msokoryansky@yahoo.com>
2017-09-04 09:55:05 -0400

    #66 solution likely correct but slow even for D <= 100

--- src/main/scala/mike/sokoryansky/EulerProblems/P0066.scala
+++ src/main/scala/mike/sokoryansky/EulerProblems/P0066.scala
@@ -2,16 +2,42 @@ package mike.sokoryansky.EulerProblems
 
 import mike.sokoryansky.MathUtils.{Diophantine, Integer}
 
+/*
+
+Consider quadratic Diophantine equations of the form:
+
+x2 – Dy2 = 1
+
+For example, when D=13, the minimal solution in x is 6492 – 13×1802 = 1.
+
+It can be assumed that there are no solutions in positive integers when D is square.
+
+By finding minimal solutions in x for D = {2, 3, 5, 6, 7}, we obtain the following:
+
+32 – 2×22 = 1
+22 – 3×12 = 1
+92 – 5×42 = 1
+52 – 6×22 = 1
+82 – 7×32 = 1
+
+Hence, by considering minimal solutions in x for D ≤ 7, the largest x is obtained when D=5.
+
+Find the value of D ≤ 1000 in minimal solutions of x for which the largest value of x is obtained.
+
+ */
+
 class P0066 extends EulerProblem {
-  def run: String = (for {
-    d <- 2 to 3
-    if !Integer.isPow(d, 2)
-    dio = Diophantine(1, 2, d, 2, 1)
-    y = dio.yFind
-    if y.nonEmpty
-    x = dio.x(y.get)
-    if x.nonEmpty
-  } yield x).max.toString
+  def run: String =
+    (1 to 100).filterNot(d => Integer.isPow(d, 2))
+      .map(d => {
+        print(s"d = $d: ")
+        val dio = Diophantine(1, 2, 0 - d, 2, 1)
+        val y = dio.yFind.get
+        print(s"y = $y, ")
+        val x = dio.x(y).get
+        println(s"x = $x")
+        x
+      }).max.toString
 }
 
 object P0066 extends App {
--- src/main/scala/mike/sokoryansky/MathUtils/Diophantine.scala
+++ src/main/scala/mike/sokoryansky/MathUtils/Diophantine.scala
@@ -7,7 +7,7 @@ case class Diophantine(a: Long, xPow: Int, b: Long, yPow: Int, c: Long) {
   def y(x: Long): Option[Long] = {
     val cMinusX = c - a * Math.pow(x, xPow).toLong
     if (cMinusX % b != 0) None
-    else if (Integer.isPow(cMinusX / b, yPow)) Some(Math.pow(cMinusX / b, 1 / yPow).toLong)
+    else if (Integer.isPow(cMinusX / b, yPow)) Some(Math.pow(cMinusX / b, 1.toDouble / yPow).toLong)
     else None
   }
   def yFind(ys: Stream[Long]): Option[Long] = ys.find(y => x(y).nonEmpty)
@@ -16,7 +16,7 @@ case class Diophantine(a: Long, xPow: Int, b: Long, yPow: Int, c: Long) {
   def x(y: Long): Option[Long] = {
     val cMinusY = c - b * Math.pow(y, yPow).toLong
     if (cMinusY % a != 0) None
-    else if (Integer.isPow(cMinusY / a, xPow)) Some(Math.pow(cMinusY / a, 1 / xPow).toLong)
+    else if (Integer.isPow(cMinusY / a, xPow)) Some(Math.pow(cMinusY / a, 1.toDouble / xPow).toLong)
     else None
   }
   def xFind(xs: Stream[Long]): Option[Long] = xs.find(x => y(x).nonEmpty)
